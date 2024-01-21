455291c0ecbc8e5ae69901ac884c8457fd3ed8e3
nzaeta <nicocpla@hotmail.com>
2024-01-20 23:21:24 -0300

    commit final

--- src/main/java/com/ayi/EjercicioEvaluativo1/service/contracts/IUsuarioService.java
+++ src/main/java/com/ayi/EjercicioEvaluativo1/service/contracts/IUsuarioService.java
@@ -1,23 +1,18 @@
 package com.ayi.EjercicioEvaluativo1.service.contracts;
 
 import com.ayi.EjercicioEvaluativo1.entity.Usuario;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 
 public interface IUsuarioService {
 
 
-    @Transactional
     Usuario save(Usuario usuario);
 
-    @Transactional
     Usuario update(Integer id, Usuario usuario);
 
-    @Transactional
     Usuario findById(Usuario usuario);
 
-    @Transactional
     void delete(Usuario usuario);
 
 
